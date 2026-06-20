c42506e74d1d8b5d324abf2f899870b2a15dcfec
Ranjeet Mishra <41523296+ranjeet-mishra-ng@users.noreply.github.com>
2026-06-20 16:54:03 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,91 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # Approach 1
+        # n = len(s)
+        # m = len(t)
+
+        # if n != m:
+        #     return False
+
+        # count_s = {}
+        # count_t = {}
+
+        # for c in s:
+        #     if c not in count_s:
+        #         count_s[c] = 1
+        #     else:
+        #         count_s[c] += 1
+        
+        # for c in t:
+        #     if c not in count_t:
+        #         count_t[c] = 1
+        #     else:
+        #         count_t[c] += 1
+        
+        # for char in count_s:
+        #     if count_s[char] != count_t.get(char, 0):
+        #         return False
+        # return True
+
+        # Approach 2
+        # n = len(s)
+        # m = len(t)
+
+        # if n != m:
+        #     return False
+        
+        # count = {}
+        # for c in s:
+        #     if c not in count:
+        #         count[c] = 1
+        #     else:
+        #         count[c] += 1
+        
+        # for c in t:
+        #     if c not in count:
+        #         count[c] = -1
+        #     else:
+        #         count[c] -= 1
+        
+        # for char in count:
+        #     if count[char] != 0:
+        #         return False
+        # return True
+
+        # Approach 3:
+        if len(s) != len(t):
+            return False
+
+        freq = [0]*26
+
+        for char in s:
+            freq[ord(char)-97] += 1
+        
+        for char in t:
+            freq[ord(char)-97] -= 1
+        
+        for i in range(26):
+            if freq[i] != 0:
+                return False
+        return True
+
+
+            
+        
+
+
+        
+
+        
+
+
+        
+
+
+
+
+        
+
+        
+
+        
